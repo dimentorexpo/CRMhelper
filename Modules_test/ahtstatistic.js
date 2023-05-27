e27e58c59220efba6c20d9357448ea6fc1fb46fa
dimentorexpo <85435517+dimentorexpo@users.noreply.github.com>
2023-05-27 18:51:07 +0300

    Update ahtstatistic.js

--- Modules_test/ahtstatistic.js
+++ Modules_test/ahtstatistic.js
@@ -7,7 +7,7 @@ butahttimer.title = "Таймер aht"
 let CRMlogo = document.getElementsByClassName('logo');
 window.addEventListener("load", function() {
     if (ahtshowcrm == 1){
-        CRMlogo[0].style = "widht:150px"
+        CRMlogo[0].style.widht = '150px'
         CRMlogo[0].appendChild(butahttimer)
     }
 });
