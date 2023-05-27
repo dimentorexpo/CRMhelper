1aecbd52227fab12a86b03a0ddbcc4babf45dfc0
dimentorexpo <85435517+dimentorexpo@users.noreply.github.com>
2023-05-27 16:54:05 +0300

    Update ahtstatistic.js

--- Modules_test/ahtstatistic.js
+++ Modules_test/ahtstatistic.js
@@ -1,7 +1,7 @@
 let butahttimer = document.createElement('button')
 butahttimer.id = "ahttimercrm"
 butAlarmclock.classList.add('btnCRM')
-butAlarmclock.innerHTML = "00 : 00"
+butAlarmclock.innerText = "00 : 00"
 butahttimer.title = "Таймер aht"
 butahttimer.style.color = "lightgreen"
 
