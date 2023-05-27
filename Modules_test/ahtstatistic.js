23fd1f971514faef52d32f25b5d40e8296500a23
dimentorexpo <85435517+dimentorexpo@users.noreply.github.com>
2023-05-27 18:27:32 +0300

    Update ahtstatistic.js

--- Modules_test/ahtstatistic.js
+++ Modules_test/ahtstatistic.js
@@ -1,3 +1,4 @@
+let takeTaskahtBtn;
 let butahttimer = document.createElement('button')
 butahttimer.id = "ahttimercrm"
 butahttimer.classList.add('btnCRM')
@@ -7,8 +8,16 @@ butahttimer.style.color = "lightgreen"
 
 let CRMlogo = document.getElementsByClassName('logo');
 if (ahtshowcrm == 1){
-    setTimeout(function () {
-        CRMlogo[0].style = "widht:150px"
-        CRMlogo[0].appendChild(butahttimer)
-    }, 2000)  
+    setTimeout(ahtstat(), 2000)  
 }
+
+function ahtstat() {
+    CRMlogo[0].style = "widht:150px"
+    CRMlogo[0].appendChild(butahttimer)
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.location.href.indexOf('https://crm2.skyeng.ru/customer-support/start') !== -1) {
+        console.log("Открыли")
+    }
+});
