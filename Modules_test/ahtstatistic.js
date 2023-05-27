0b2c3d84af3e1506f97b28bd960f39eaed23f59d
dimentorexpo <85435517+dimentorexpo@users.noreply.github.com>
2023-05-27 19:43:33 +0300

    Update ahtstatistic.js

--- Modules_test/ahtstatistic.js
+++ Modules_test/ahtstatistic.js
@@ -1,12 +1,12 @@
-let takeTaskBtnlist;
 let butahttimer = document.createElement('button')
 butahttimer.id = "ahttimercrm"
 butahttimer.innerText = "00 : 00"
 butahttimer.title = "Таймер aht"
 
-let CRMlogo = document.getElementsByClassName('logo');
+let CRMlogo;
 window.addEventListener("load", function() {
-    setTimeout(() => {
+    CRMlogo = document.getElementsByClassName('logo');
+    setTimeout(function() {
         if (ahtshowcrm == 1){
             CRMlogo[0].style.widht = '150px'
             CRMlogo[0].appendChild(butahttimer)
@@ -14,19 +14,18 @@ window.addEventListener("load", function() {
     }, 1000);
 });
 
-function startlisteneraht(){
-    takeTaskBtnlist = document.getElementsByClassName('mat-button-wrapper');
+function startlisteneraht() {
+    var takeTaskBtnlist = document.getElementsByClassName('mat-button-wrapper');
     if (window.location.href.indexOf('https://crm2.skyeng.ru/customer-support/start') !== -1 && takeTaskBtnlist.length > 0) {
-        if (takeTaskBtnlist[13] && takeTaskBtnlist[13].innerText == 'Взять новую задачу') {
-            let TaskahtBtn = takeTaskBtnlist[13]
-            TaskahtBtn.addEventListener("click", startahttimer())    
-        }
+      if (takeTaskBtnlist[13] && takeTaskBtnlist[13].innerText == 'Взять новую задачу') {
+        var TaskahtBtn = takeTaskBtnlist[13];
+        TaskahtBtn.addEventListener("click", startahttimer);
+      }
     }
-}
-
-function startahttimer(){
-    if (document.getElementsByClassName('mat-button-disabled').length == 0){
-        console.log('КЛИК')
+  }
+  
+  function startahttimer() {
+    if (document.getElementsByClassName('mat-button-disabled').length === 0) {
+      console.log('КЛИК');
     }
-    
-}
+  }
